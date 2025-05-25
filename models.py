19e099dbea0e711fa814daa47cf1d5a636dae507
YSH <ysh5586@naver.com>
2025-05-26 00:10:51 +0900

    [YSH / 2025-05-25] - api.py -> redirect_short_url description 변경 - models.py -> short_url, long_url unique 속성 추가

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ class URL(Base):
     __tablename__ = "urls"
 
     id = Column(Integer, primary_key=True, index=True)
-    short_url = Column(String(10), index=True)
-    long_url = Column(String(2000), index=True)
+    short_url = Column(String(10), index=True, unique=True)
+    long_url = Column(String(2000), index=True, unique=True)
 
     created_at = Column(DateTime, default=lambda : datetime.now(timezone.utc).replace(tzinfo=None), nullable=False)
